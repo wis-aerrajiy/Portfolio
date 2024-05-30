297df7a6989ab918627d27eb7c0bd5f905a1bd5c
wis-aerrajiy <frabce25@gmail.com>
2024-05-30 21:51:03 +0100

    tldraw design

--- src/pages/Components/AboutSection.tsx
+++ src/pages/Components/AboutSection.tsx
@@ -3,24 +3,24 @@ import dabous from '../../assets/dabous.png';
 import '../../index.css';
 // import { BreadCrumb } from './';
 
-// import { FaGithub } from "react-icons/fa";
-// import { SiLinkedin } from "react-icons/si";
-// import { FaTwitter } from "react-icons/fa";
-// import { FaPhoneAlt } from "react-icons/fa";
-// import { MdEmail } from "react-icons/md";
+import { FaGithub } from "react-icons/fa";
+import { SiLinkedin } from "react-icons/si";
+import { FaTwitter } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 import Draggable from 'react-draggable';
 // import { CSSTransition } from 'react-transition-group';
 
-// const socials = [
-//     { id: 0, link: '', icon: FaGithub },
-//     { id: 1, link: '', icon: SiLinkedin },
-//     { id: 2, link: '', icon: FaTwitter },
-// ];
+const socials = [
+    { id: 0, link: '', icon: FaGithub },
+    { id: 1, link: '', icon: SiLinkedin },
+    { id: 2, link: '', icon: FaTwitter },
+];
 
-// const PersonalInfo = [
-//     { id: 0, icon: FaPhoneAlt, info: '+212 658506376' },
-//     { id: 1, icon: MdEmail, info: 'aerrajiy.intra@gmail.com' },
-// ]
+const PersonalInfo = [
+    { id: 0, icon: FaPhoneAlt, info: '+212 658506376' },
+    { id: 1, icon: MdEmail, info: 'aerrajiy.intra@gmail.com' },
+]
 
 const AboutSection = () => {
     return (
@@ -28,14 +28,16 @@ const AboutSection = () => {
             <section id="about">
                 <div className='flex flex-col lg:flex-row justify-center items-center min-h-screen space-y-5 md:space-y-0 lg:space-x-50 mt-24 lg:mt-0'>
 
-                    <div className='flex md:flex-col justify-start items-center h-[auto] w-[90vw] lg:w-[auto] lg:h-[80vh] max-w-[90vw] ring-1 ring-gray-200 shadow-sm rounded-lg px-10 mx-1' >
+                    <div className=' grid grid-cols-1 h-[auto] w-[90vw] lg:w-[auto] lg:h-[80vh] max-w-[90vw] ring-1 ring-gray-200 shadow-sm rounded-lg px-10 mx-1' >
                         <div className='max-w-[90vw]'>
-                            <div className='relative -z-1'>
-                                <img
-                                    src="https://badge.mediaplus.ma/darkblue/aerrajiy"
-                                    alt="profile"
-                                    className='rounded-lg mt-10 w-[100%] object-cover rotate-[5deg] filter grayscale contrast-150 shadow-2xl' />
-                                <img src={dabous} alt="dabous" className='absolute -top-4 left-[40%] md:left-[45%] rounded-lg w-[50px] filter  contrast-150 ' />
+                            <div className="stickyNote bg-[#A9F4DB]/40 !rotate-[-3deg]">
+                                <div className='relative -z-1 m-5'>
+                                    <img
+                                        src="https://badge.mediaplus.ma/darkblue/aerrajiy"
+                                        alt="profile"
+                                        className='rounded-lg mt-10 w-[100%] object-cover rotate-[5deg] filter opacity-90 shadow-2xl' />
+                                    <img src={dabous} alt="dabous" className='absolute -top-4 left-[40%] md:left-[45%] rounded-lg w-[50px] filter  contrast-150 ' />
+                                </div>
                             </div>
 
                             <h3 className="text-2xl md:text-4xl text-blue-900 underline mt-10">
@@ -44,33 +46,30 @@ const AboutSection = () => {
                             <p className="text-lg md:text-lg text-slate-500 ml-20">
                                 Else Known As <span className="text-blue-900 underline-offset-8 underline px-3">Wis</span>
                             </p>
-
                         </div>
-                        {
-                            /* 
-                            <div>
-                                <ul className='flex justify-between items-center space-x-20 mt-10'>
-                                    {
-                                        socials.map(item => (
-                                            <li key={item.id} className='p-4 rounded-lg flex items-center border border-gray-300 justify-center group transition-all duration-500 hover:-translate-y-2'> <item.icon /> </li>
-                                        ))
-                                    }
-                                </ul>
-                            </div>
-    
-                            <div className=' mt-10 max-w-[100%]'>
-                                <ul className='flex flex-col justify-between items-center gap-5'>
-                                    {
-                                        PersonalInfo.map(item => (
-                                            <li key={item.id} className='m-5 w-full p-4 rounded-lg flex flex-row items-center border border-gray-300 justify-center group transition-all duration-500 hover:-translate-y-2'>
-                                                <item.icon className='mx-3' />
-                                                <span>{item.info}</span>
-                                            </li>
-                                        ))
-                                    }
-                                </ul>
-                            </div> */
-                        }
+
+                        <div className=''>
+                            <ul className='flex justify-between items-center space-x-20 mt-10'>
+                                {
+                                    socials.map(item => (
+                                        <li key={item.id} className='p-4 rounded-lg flex items-center border border-gray-300 justify-center group transition-all duration-500 hover:-translate-y-2'> <item.icon /> </li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
+
+                        <div className=' mt-10 max-w-[100%]'>
+                            <ul className='flex flex-col justify-between items-center gap-5'>
+                                {
+                                    PersonalInfo.map(item => (
+                                        <li key={item.id} className='m-5 w-full p-4 rounded-lg flex flex-row items-center border border-gray-300 justify-center group transition-all duration-500 hover:-translate-y-2'>
+                                            <item.icon className='mx-3' />
+                                            <span>{item.info}</span>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
                     </div>
 
                     <div
