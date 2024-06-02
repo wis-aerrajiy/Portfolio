924337e57ca5f0f948c46f34d4043e6e4ce62f18
wis-aerrajiy <frabce25@gmail.com>
2024-06-02 03:24:42 +0100

    about done

--- src/pages/Components/AboutSection.tsx
+++ src/pages/Components/AboutSection.tsx
@@ -14,7 +14,7 @@ import { ImCheckmark } from "react-icons/im";
 
 import Draggable from 'react-draggable';
 
-// import React from 'react';
+import React from 'react';
 // import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 // const data = [
@@ -56,6 +56,25 @@ import Draggable from 'react-draggable';
 //     );
 // }
 
+/* Skills Icons */
+import { SiTypescript } from "react-icons/si";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaPython } from "react-icons/fa";
+import { TbBrandCpp } from "react-icons/tb";
+import { FaReact } from "react-icons/fa";
+import { FaNodeJs } from "react-icons/fa";
+import { SiNestjs } from "react-icons/si";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { SiMongodb } from "react-icons/si";
+import { BiLogoPostgresql } from "react-icons/bi";
+import { PiDevToLogoFill } from "react-icons/pi";
+
+/* Skills Icons */
+import { FaDocker } from "react-icons/fa";
+import { FaGitAlt } from "react-icons/fa";
+import { SiSocketdotio } from "react-icons/si";
+import { SiAzuredevops } from "react-icons/si";
+
 const socials = [
     { id: 0, link: '', icon: FaGithub },
     { id: 1, link: '', icon: SiLinkedin },
@@ -67,14 +86,58 @@ const PersonalInfo = [
     { id: 1, icon: MdEmail, info: 'aerrajiy.intra@gmail.com' },
 ];
 
-const Skills = [
-    { "id": 0, "name": "HTML5", "level": "90%", "icon": ImCheckmark },
-    { "id": 1, "name": "CSS3", "level": "85%", "icon": ImCheckmark },
-    { "id": 2, "name": "JavaScript", "level": "80%", "icon": ImCheckmark },
-    { "id": 3, "name": "React", "level": "75%", "icon": ImCheckmark },
-    { "id": 4, "name": "Node.js", "level": "70%", "icon": ImCheckmark },
-    { "id": 5, "name": "Express.js", "level": "70%", "icon": ImCheckmark },
-]
+
+// hard coded skills
+const TechnicalSkills = [
+    { id: 0, name: 'WebDev', level: '80%', icon: PiDevToLogoFill },
+    { id: 1, name: 'TypeScript', level: '80%', icon: SiTypescript },
+    { id: 2, name: 'JavaScript', level: '80%', icon: IoLogoJavascript },
+    { id: 3, name: 'C++', level: '80%', icon: TbBrandCpp },
+    { id: 4, name: 'Python', level: '70%', icon: FaPython },
+    { id: 5, name: 'React', level: '80%', icon: FaReact },
+    { id: 6, name: 'Node.js', level: '80%', icon: FaNodeJs },
+    { id: 7, name: 'Nest.js', level: '70%', icon: SiNestjs },
+    { id: 8, name: 'TailwindCSS', level: '80%', icon: RiTailwindCssFill },
+    { id: 9, name: 'MongoDB', level: '80%', icon: SiMongodb },
+    { id: 10, name: 'PostgreSQL', level: '70%', icon: BiLogoPostgresql },
+];
+
+const OtherSkills = [
+    { id: 0, name: 'Docker', level: '80%', icon: FaDocker },
+    { id: 1, name: 'VC Git', level: '80%', icon: FaGitAlt },
+    { id: 2, name: 'Socket.io', level: '80%', icon: SiSocketdotio },
+    { id: 3, name: 'DevOps', level: '80%', icon: SiAzuredevops },
+    { id: 4, name: 'Problem Solving', level: '80%', icon: ImCheckmark },
+    { id: 5, name: 'Teamwork', level: '80%', icon: ImCheckmark },
+    { id: 6, name: 'Communication', level: '80%', icon: ImCheckmark },
+    { id: 7, name: 'Time Management', level: '80%', icon: ImCheckmark },
+    { id: 8, name: 'Leadership', level: '80%', icon: ImCheckmark },
+    { id: 9, name: 'Creativity', level: '80%', icon: ImCheckmark },
+];
+
+
+const Skills = TechnicalSkills
+
+const Certificates = [
+    {
+        id: 0,
+        src: ' 1337',
+        name: 'digital technology architect at 1337',
+        link: 'https://www.1337.ma/'
+    },
+    {
+        id: 1,
+        src: ' Edx.org',
+        name: 'Google Cloud Computing Foundations at Coursera Edx.org Google Cloud',
+        link: 'https://courses.edx.org/dashboard/programs/1fcffef0-468a-483f-95aa-87e2d4d2c408/?_gl=1*1du810v*_ga*NDA3NzczOTguMTcxNjQ4ODE4Ng..*_ga_D3KS4KMDT0*MTcxNzI5MjY1Mi42LjAuMTcxNzI5MjY1Mi42MC4wLjA.'
+    },
+    {
+        id: 2,
+        src: ' Edx.org',
+        name: 'DevOps, Cloud, and Agile Foundations at Edx.org IBM',
+        link: 'https://courses.edx.org/dashboard/programs/950b68bb-50db-445c-b271-5bd46648836c/?_gl=1*f5fsq0*_ga*NDA3NzczOTguMTcxNjQ4ODE4Ng..*_ga_D3KS4KMDT0*MTcxNzI5MjY1Mi42LjAuMTcxNzI5MjY1Mi42MC4wLjA.'
+    },
+];
 
 interface SkillProps {
     skill: {
@@ -87,14 +150,14 @@ interface SkillProps {
 
 const SkillComponent = ({ skill }: SkillProps) => {
     return (
-        <div className="relative flex flex-row justify-between items-center px-4 py-2 border-b-2 border-red-800">
+        <div className="relative flex flex-row justify-between items-center px-4 py-1">
             <div
                 className={`absolute left-11 top-1.5 noise min-h-[80%] -z-20 rounded-full`}
                 style={{ width: skill.level }}
             ></div>
             <skill.icon className='text-4xl p-2 rounded-lg border border-gray-300' />
-            <div className='flex flex-col items-start font-gochi text-2xl text-[#1E3A8A] ring-offset-opacity-50 bg-[#EAF9D0] px-2'>
-                <span className='font-gochi text-x'>{skill.name}</span>
+            <div className='flex flex-col items-start font-gochi text-2xl text-slate-700 ring-offset-opacity-50 bg-[#EAF9D0] px-1'>
+                <span className='font-gochi md:text-xl text-sm !font-bold'>{skill.name}</span>
             </div>
             <div className='flex flex-row items-center'>
                 <span
@@ -108,12 +171,22 @@ const SkillComponent = ({ skill }: SkillProps) => {
 }
 
 const AboutSection = () => {
+    const [quote, setQuote] = React.useState('');
+
+    React.useEffect(() => {
+        fetch('https://api.quotable.io/quotes/random?tags=technology,famous-quotes')
+            .then(response => response.json())
+            .then(data => {
+                setQuote((data && data[0]) ? data[0].content : 'No Quote Found');
+            });
+    }, []);
+
     return (
         <>
             <section id="about">
-                <div className='flex flex-col lg:flex-row justify-center items-center min-h-screen space-y-5 md:space-y-0 lg:space-x-50 mt-24 lg:mt-0'>
+                <div className='flex flex-col lg:flex-row justify-center items-center min-h-screen space-y-5 md:space-y-24 lg:space-y-10 lg:space-x-50 mt-24 lg:mt-0'>
 
-                    <div className='grid grid-cols-1 h-[auto] w-[90vw] lg:w-[auto] md:h-[90vh] max-w-[90vw] rounded-lg m-auto' >
+                    <div className='grid grid-cols-1 h-[auto] w-[90vw] lg:w-[auto] md:h-[90vh] max-w-[90vw] rounded-lg md:m-24 lg:m-auto' >
                         <div className='mx-5'>
                             <div className="stickyNote bg-[#A9F4DB]/40 !rotate-[-3deg]">
                                 <div className='relative -z-1 ring-1 ring-gray-600/15'>
@@ -151,14 +224,15 @@ const AboutSection = () => {
                                             ))
                                         }
                                     </ul>
-
                                     <div className=' mt-10 max-w-[100%]'>
                                         <ul className='flex flex-col justify-between items-center gap-5'>
                                             {
                                                 PersonalInfo.map(item => (
-                                                    <li key={item.id} className='m-5 w-full p-4 rounded-lg flex flex-row items-center border border-gray-300 justify-center group transition-all duration-500 hover:-translate-y-2'>
-                                                        <item.icon className='mx-3' />
-                                                        <span>{item.info}</span>
+                                                    <li key={item.id} className='m-2 w-full p-4 rounded-lg flex flex-row items-center border border-gray-300 group transition-all duration-500 hover:-translate-y-2'>
+                                                        <item.icon className='hidden md:block mx-3' />
+                                                        <span
+                                                            className='font-gochi text-slate-700 ring-offset-opacity-50'
+                                                        >{item.info}</span>
                                                     </li>
                                                 ))
                                             }
@@ -175,9 +249,7 @@ const AboutSection = () => {
                         className='relative grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 md:gap-8 lg:grid-cols-2 md:overflow-scroll myscrollbar max-w-[90vw] min-w-[90vw] lg:min-w-[50vw] lg:max-w-[50vw] md:h-[90vh]  rounded-lg  p-5 mx-5'
                     >
 
-                        <Draggable
-                            disabled={true}
-                        >
+                        {/* <Draggable disabled={true} > */}
                             <div
                                 className="flex flex-col stickyNote taped space-y-5 shadow-md ring ring-red-600 bg-[#F6EDA9]/55 md:!rotate-[-3deg]"
                             >
@@ -191,18 +263,33 @@ const AboutSection = () => {
                                         Specializing in Back-end development, database management, and Dev-Ops workflow.
                                         I thrive on creative problem-solving. I am dedicated to crafting innovative digital solutions.
                                     </p>
+
+                                    <ul className='flex flex-col gap-3'>
+                                        <li className='flex flex-row items-center gap-2'>
+                                            <ImCheckmark className='text-2xl text-black/45' />
+                                            <span className='text-lg text-slate-500 font-gochi'>Highly motivated and eager to learn new technologies.</span>
+                                        </li>
+                                        <li className='flex flex-row items-center gap-2'>
+                                            <ImCheckmark className='text-2xl text-black/45' />
+                                            <span className='text-lg text-slate-500 font-gochi'>Strong problem-solving skills and a passion for software development.</span>
+                                        </li>
+                                        <li className='flex flex-row items-center gap-2'>
+                                            <ImCheckmark className='text-2xl text-black/45' />
+                                            <span className='text-lg text-slate-500 font-gochi'>Excellent communication and teamwork skills.</span>
+                                        </li>
+                                    </ul>
                                 </div>
                             </div>
 
-                        </Draggable>
+                        {/* </Draggable> */}
 
                         <div className="flex flex-col stickyNote taped space-y-5 bg-[#F0F0F0]/55 md:!rotate-[-3deg]">
                             <div className='flex justify-center items-center w-full'>
-                                <h1 className='underline underline-offset-4 font-extrabold text-2xl md:rotate-[0deg] font-solway' >My Journey</h1>
+                                <h1 className='underline underline-offset-4 font-extrabold text-2xl md:rotate-[0deg] font-gochi' >My Journey</h1>
                             </div>
 
                             <div className="flex flex-col">
-                                <p className="text-lg text-slate-500 text-justify font-serif font-solway">
+                                <p className="text-lg text-slate-500 text-justify font-gochi">
                                     with over 2 years of study in computer science and software develepment, I have developed a strong understanding of software development and computer science fundamentals.
                                 </p>
                             </div>
@@ -211,21 +298,21 @@ const AboutSection = () => {
                         <div
                             className="
                             md:col-start-1 md:col-span-1 md:row-start-2 md:row-span-1
-                            flex flex-col stickyNote taped space-y-5 bg-[#D9F5AA]/55 md:rotate-[3deg]"
+                            flex flex-col stickyNote taped space-y-3 bg-[#D9F5AA]/55 md:rotate-[3deg]"
                         >
                             <div className='flex justify-center items-center w-full'>
                                 <h1 className='underline underline-offset-4 font-extrabold text-2xl md:rotate-[0deg] font-gochi' >My Skilles</h1>
                             </div>
 
-                            <Draggable>
+                            {/* <Draggable> */}
                                 <div className='flex flex-row gap-5 justify-center items-center bg-slate-700/40 mx-auto px-6 py-3 rounded-md'>
                                     <RxDownload /> Download Resume
                                 </div>
-                            </Draggable>
+                            {/* </Draggable> */}
 
-                            <div className="grid grid-cols-1 gap-3 md:gap-7 ">
+                            <div className="grid grid-cols-1 gap-1 md:gap-2 ">
                                 {
-                                    Skills.map(skill => (
+                                    TechnicalSkills.map(skill => (
                                         <SkillComponent key={skill.id} skill={skill} />
                                     ))
                                 }
@@ -235,15 +322,15 @@ const AboutSection = () => {
 
                         <div
                             className="
-                            flex flex-col stickyNote taped space-y-5 bg-[#D9F5AA]/55 md:rotate-[3deg]"
+                            flex flex-col stickyNote taped space-y-1 bg-[#D9F5AA]/55 md:rotate-[3deg]"
                         >
                             <div className='flex justify-center items-center w-full'>
                                 <h1 className='underline underline-offset-4 font-extrabold text-2xl md:rotate-[0deg] font-gochi' >My Skilles</h1>
                             </div>
 
-                            <div className="grid grid-cols-1 gap-3 md:gap-7 ">
+                            <div className="grid grid-cols-1 gap-3 md:gap-5 ">
                                 {
-                                    Skills.map(skill => (
+                                    OtherSkills.map(skill => (
                                         <SkillComponent key={skill.id} skill={skill} />
                                     ))
                                 }
@@ -267,21 +354,45 @@ const AboutSection = () => {
                                 <h1 className='underline underline-offset-4 font-extrabold text-2xl md:rotate-[0deg] font-gochi' >Education & Certifications</h1>
                             </div>
 
-                            <div className="flex flex-col">
+                            <div className="flex flex-col gap-5">
                                 <p className="text-lg text-slate-500 text-justify font-gochi">
-                                    As a dynamic junior full stack developer, I have a proven track record of delivering high-quality, scalable and maintainable software solutions. I have a strong understanding of web technologies and I am passionate about learning new technologies and frameworks.
+                                    A digital technology architect with a passion for software development at 1337 Coding School.
+                                    <br />
+                                    <span className='font-bold text-slate-500'>Certifications:</span>
+
+
                                 </p>
+
+                                <ul className='flex flex-col gap-3'>
+
+                                    {
+                                        Certificates.map(cert => (
+                                            <li className='flex flex-row items-center gap-2' key={cert.id}>
+                                                <ImCheckmark className='text-2xl text-black/45' />
+                                                <span className='text-lg text-slate-500 font-gochi'> {cert.name} at
+                                                    <a
+                                                        href={cert.link}
+                                                        target="_blank" rel="noreferrer"
+                                                        className='text-blue-600 underline'
+                                                    >
+                                                        {cert.src}
+                                                    </a>
+                                                </span>
+                                            </li>
+                                        ))
+                                    }
+                                </ul>
                             </div>
                         </div>
 
                         <div className="flex flex-col stickyNote taped space-y-5 bg-[#C0D2F5]/55">
                             <div className='flex justify-center items-center w-full'>
-                                <h1 className='underline underline-offset-4 font-extrabold text-2xl md:rotate-[0deg] font-gochi' >About Me 6</h1>
+                                <h1 className='underline underline-offset-4 font-extrabold text-2xl md:rotate-[0deg] font-gochi' >Random Quote</h1>
                             </div>
 
                             <div className="flex flex-col">
                                 <p className="text-lg text-slate-500 text-justify font-gochi">
-                                    As a dynamic junior full stack developer, I have a proven track record of delivering high-quality, scalable and maintainable software solutions. I have a strong understanding of web technologies and I am passionate about learning new technologies and frameworks.
+                                    {quote}
                                 </p>
                             </div>
                         </div>
