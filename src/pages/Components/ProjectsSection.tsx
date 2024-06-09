c4a6bb1cd3355ba60feca4cab3314b400ae64baa
wis-aerrajiy <frabce25@gmail.com>
2024-06-09 22:49:10 +0100

    deploy to vercel

--- src/pages/Components/ProjectsSection.tsx
+++ src/pages/Components/ProjectsSection.tsx
@@ -1,3 +1,5 @@
+import { Slide } from "react-awesome-reveal";
+
 const ProjectsSection = () => {
     const projects: any = [];
 
@@ -13,44 +15,48 @@ const ProjectsSection = () => {
     return (
         <section id="projects">
             <div className="flex flex-col justify-center items-center min-h-screen max-w-[90vw] min-w-[90vw] xl:min-w-[90vw] xl:max-w-[40vw] rounded-lg  p-5 lg:mx-5">
- 
+
                 <div className="flex flex-col justify-between items-center max-w-[95vw] min-w-[95vw] xl:max-w-[70vw] xl:min-w-[70vw] md:max-w-[90vw] md:min-w-[90vw] mt-24">
 
                     {
                         Array.from({ length: 6 }).map((_, i) => (
-                            <div
-                                className={`
+                            <Slide key={i} direction={`${(i % 2) ? 'left' : 'right'}`} triggerOnce delay={i * 100} damping={0.1} className="">
+
+                                <div
+                                    className={`
                                     bg-slate-500/30 m-5 rounded-md
                                     flex flex-col-reverse
                                     ${(i % 2 === 0 ? 'md:flex-row' : 'md:flex-row-reverse')}
                                     justify-between items-center space-y-3
                                     filter drop-shadow-lg
                                     hover:filter-none hover:drop-shadow-2xl
+                                    p-5 relative hover:scale-95 hover:duration-500 hover:ease-in-out hover:-z-30
                                     `
-                                }>
-                                <div
-                                    className="rounded-lg p-5 max-w-[20rem]"
-                                >
-                                    <img
-                                        src="https://42.fr/wp-content/uploads/2021/07/Transcendence-e1629765085487.jpg"
-                                        alt="project"
-                                        className="rounded-lg w-[100%] object-cover shadow-2xl" />
-                                </div>
+                                    }>
 
-                                <div className="flex flex-col justify-between items-start space-y-6 p-5 xl:max-w-[70%] lg:max-w-[90%] ">
-
-                                    <h5 className="text-2xl font-gochi">Projects</h5>
-
-                                    <div className="flex justify-around items-start space-x-10 ">
-                                        <p className="font-gochi"> Title </p>
-                                        <p className="font-gochi"> Date </p>
+                                    <div
+                                        className="rounded-lg p-5 max-w-[20rem]"
+                                    >
+                                        <img
+                                            src="https://42.fr/wp-content/uploads/2021/07/Transcendence-e1629765085487.jpg"
+                                            alt="project"
+                                            className="rounded-lg w-[100%] object-cover shadow-2xl" />
                                     </div>
 
-                                    <p className="font-gochi">
-                                        Voici un récap de mon cursus à l'école 42, des projects effectués, des notes obtenues et du code sources, HF ! Je suis sur le campus de 42 Lyon, de la promo 2021, piscine d'Août (objectivement la meilleure) !
-                                    </p>
+                                    <div className="flex flex-col justify-between items-start space-y-6 p-5 xl:max-w-[70%] lg:max-w-[90%] ">
+
+                                        <h5 className="text-2xl font-gochi">Projects</h5>
+
+                                        <div className="flex justify-around items-start space-x-10 ">
+                                            <p className="font-gochi"> Date </p>
+                                        </div>
+
+                                        <p className="font-gochi">
+                                            Voici un récap de mon cursus à l'école 42, des projects effectués, des notes obtenues et du code sources, HF ! Je suis sur le campus de 42 Lyon, de la promo 2021, piscine d'Août (objectivement la meilleure) !
+                                        </p>
+                                    </div>
                                 </div>
-                            </div>
+                            </Slide>
                         ))
                     }
                 </div>
